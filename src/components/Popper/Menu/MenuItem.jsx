08675dc60c502f4bee31743a5ac6a9e6d6659e33
Hoang Tien Dat <hoangtiendat.thidaihoc@gmail.com>
2022-09-17 11:34:52 +0700

    update

--- src/components/Popper/Menu/MenuItem.jsx
+++ src/components/Popper/Menu/MenuItem.jsx
@@ -5,17 +5,12 @@ import AppButton from '~/components/Layout/components/AppButton/AppButton';
 
 const cx = classNames.bind(styles);
 
-function MenuItem({ data, onClick}) {
-
+function MenuItem({ data, onClick }) {
     const classes = cx('menu-item', {
-        separate : data.separate
-    })
+        separate: data.separate,
+    });
     return (
-        <AppButton 
-        onClick={onClick}
-        className={classes} 
-        leftIcon={data.icon} 
-        to={data.to}>
+        <AppButton onClick={onClick} className={classes} leftIcon={data.icon} to={data.to}>
             {data.title}
         </AppButton>
     );
